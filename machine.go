47ac18d7d44a02413bc68484a71522f41604f81c
Michael Rybka <merybka@gmail.com>
2023-10-04 18:12:01 -0400

    Add NewMachine func

--- machine.go
+++ machine.go
@@ -1,5 +1,12 @@
 package remote
 
+func NewMachine(userID, host string) *Machine {
+	return &Machine{
+		UserID: userID,
+		Host:   host,
+	}
+}
+
 type Machine struct {
 	UserID string
 	Host   string
